56b38dd9aa2a817b96c75337805dd4512bcb7179
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-08 11:19:46 +0900

    github workflow

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,18 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-  let prisma: PrismaClient | undefined;
+    var prisma: PrismaClient;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
